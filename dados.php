0f2a5b3cc6d1b063547172025020f1eae14cb7e4
Seu Nome <seu.email@example.com>
2025-01-30 12:35:20 -0300

    Entendendo sobre MVC e criando primeiras Views

---
+++ dados.php
@@ -0,0 +1,14 @@
+<?php
+
+$livros = [
+    ['id' => 1, 'titulo' => 'Senhor dos Anéis', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'Uma épica aventura em um mundo de fantasia onde um hobbit deve destruir um anel maligno para salvar a Terra-Média'],
+    ['id' => 2, 'titulo' => 'O Hobbit', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'A jornada de Bilbo Bolseiro em uma missão inesperada com um grupo de anões para recuperar um tesouro guardado por um dragão'],
+    ['id' => 3, 'titulo' => 'Harry Potter e a Pedra Filosofal', 'autor' => 'J.K. Rowling', 'descricao' => 'Um jovem bruxo descobre seu destino ao ingressar em uma escola de magia, onde enfrenta desafios e descobre segredos sobre seu passado'],
+    ['id' => 4, 'titulo' => 'Crônicas de Nárnia', 'autor' => 'C.S. Lewis', 'descricao' => 'Crianças descobrem um mundo mágico através de um guarda-roupa e se envolvem em aventuras fantásticas para salvar o reino de Nárnia'],
+    ['id' => 5, 'titulo' => 'Dom Quixote', 'autor' => 'Miguel de Cervantes', 'descricao' => 'As aventuras de um fidalgo que, após ler muitos romances de cavalaria, decide se tornar um cavaleiro andante em busca de glória'],
+    ['id' => 6, 'titulo' => '1984', 'autor' => 'George Orwell', 'descricao' => 'Uma distopia que retrata um futuro totalitário onde a sociedade vive sob constante vigilância e controle do governo'],
+    ['id' => 7, 'titulo' => 'Crime e Castigo', 'autor' => 'Fiódor Dostoiévski', 'descricao' => 'Um estudante comete um assassinato e enfrenta um intenso conflito moral e psicológico enquanto lida com as consequências de seus atos'],
+    ['id' => 8, 'titulo' => 'Cem Anos de Solidão', 'autor' => 'Gabriel García Márquez', 'descricao' => 'A saga da família Buendía ao longo de várias gerações em Macondo, mesclando realidade e elementos mágicos'],
+    ['id' => 9, 'titulo' => 'A Metamorfose', 'autor' => 'Franz Kafka', 'descricao' => 'Um caixeiro viajante acorda certa manhã transformado em um inseto gigante, explorando temas de alienação e absurdo']
+]
+;
---
+++ views/index.views.php
@@ -0,0 +1,28 @@
+<form class="w-full flex space-x-2 mt-6">
+    <input type="text" class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1" placeholder="Pesquisar...">
+    <button type="submit">🔍</button>
+</form>
+
+<!--Section de Livros-->
+
+<section class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+
+    <!--Livros-->
+    <?php foreach ($livros as $livro): ?> <!--Loop para todos os livros-->
+        <div class="p-2 rounded  bg-stone-900 border-stone-800 border-2">
+            <div class="flex"> <!--Div Pai que contém a Imagem e as Infos Iniciais do Livro-->
+                <div class="w-1/3">Imagem</div> <!--Imagem do Livro-->
+                <div class="space-y-2"><!--Bloco as Informações iniciais do Livro-->
+                    <a href="/livro.php?id=<?= $livro['id'] ?>" class="font-semibold hover:underline"><?= $livro['titulo'] ?></a>
+                    <div class="text-xs italic"><?= $livro['autor'] ?></div>
+                    <div class="text-xs italic">⭐⭐⭐⭐⭐(3Avaliações)</div>
+                </div>
+            </div>
+            <div class="mt-2"> <!--Descrição dos Livros-->
+                <?= $livro['descricao'] ?>
+            </div>
+        </div>
+
+    <?php endforeach; ?>
+
+</section>
