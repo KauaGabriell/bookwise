72676f5e370bb9e4c07749f66c7891e9e659ef8e
Seu Nome <seu.email@example.com>
2025-01-30 09:40:38 -0300

    Criando a barra de navegação

--- index.php
+++ index.php
@@ -6,7 +6,24 @@
     <title>BookWise</title>
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 </head>
-<body>
-    
+<body class="bg-stone-950 text-stone-200">
+
+    <header class="bg-stone-900 shadow-lg">
+
+        <!--Barra de Navegação-->
+        <nav class="mx-auto max-w-screen-lg px-8 py-4 flex justify-between">
+            <div class="font-bold text-xl tracking-wide">Book Wise</div>
+            <ul class="flex space-x-4 font-bold">
+                <li><a href="#" class="text-lime-500">Explorar</a></li>
+                <li><a href="/meus-livros.php" class="hover:underline">Meus Livros</a></li>
+            </ul>
+
+            <ul>
+                <li><a href="/login.php" class="hover:underline">Fazer Login</a></li>
+            </ul>
+        </nav>
+
+    </header>
+
 </body>
 </html>
